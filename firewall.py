602eba398698aa0c60da8d92e6d625905635db63
Michael Ho <michaelho2016@berkeley.edu>
2014-11-15 15:04:03 -0800

    initial

---
+++ firewall.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from main import PKT_DIR_INCOMING, PKT_DIR_OUTGOING
+
+# TODO: Feel free to import any Python standard moduless as necessary.
+# (http://docs.python.org/2/library/)
+# You must NOT use any 3rd-party libraries, though.
+
+class Firewall:
+    def __init__(self, config, iface_int, iface_ext):
+        self.iface_int = iface_int
+        self.iface_ext = iface_ext
+
+        # TODO: Load the firewall rules (from rule_filename) here.
+        print 'I am supposed to load rules from %s, but I am feeling lazy.' % \
+                config['rule']
+
+        # TODO: Load the GeoIP DB ('geoipdb.txt') as well.
+        # TODO: Also do some initialization if needed.
+
+    # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
+    # @pkt: the actual data of the IPv4 packet (including IP header)
+    def handle_packet(self, pkt_dir, pkt):
+        # TODO: Your main firewall code will be here.
+        pass
+
+    # TODO: You can add more methods as you want.
+
+# TODO: You may want to add more classes/functions as well.
