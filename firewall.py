59dae28990531e49c9c35eb2ede7e030de5df9df
Romi Phadte <romiphadte@gmail.com>
2014-11-29 23:02:24 -0800

    duh syntax error

--- firewall.py
+++ firewall.py
@@ -69,7 +69,7 @@ class Firewall:
             self.pass_packet(pkt,pkt_dir)
             return
         ip_header_len,protocol=(struct.unpack('!B',pkt[0:1])[0]&0xF)*4,struct.unpack('!B',pkt[9:10])[0]
-        if len(pkt)<8+ip_header_len or (protocol==17 and len(pkt)<20+ip_header_len)
+        if len(pkt)<8+ip_header_len or (protocol==17 and len(pkt)<20+ip_header_len):
             return
         ip=""
         src_ip=pkt[12:16]
